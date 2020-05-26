87a484867b0d65e56142e937f8d362b9badd9328
unknown <srinivas.maroju@hotmail.com>
2020-05-26 18:24:50 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+/*
+  { path: 'comp1',
+    loadChildren: () => import('./module1/module1.module').then((m) => m.Module1Module)},
+  { path: 'comp2',
+    loadChildren: () => import('./module2/module2.module').then((m) => m.Module2Module)},
+*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Module1Module } from './module1/module1.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { Module2Module } from './module2/module2.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    Module1Module,
+    Module2Module,
     AppRoutingModule
   ],
   providers: [],
